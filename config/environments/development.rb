5c8217c3970fee115618eb79874188c865210f8c
mayank <kumarmayankdev2@gmail.com>
2019-04-01 20:36:24 +0530

    load serializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.paths.add "app/serializers/concerns", eager_load: true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
